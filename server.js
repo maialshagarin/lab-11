84e6b10d83fab1f257f324c5a848b7234ee8c703
muna alshorman <munaalshorman123@gmail.com>
2019-11-25 13:05:20 +0200

    task3 is not completed

--- server.js
+++ server.js
@@ -1,20 +1,56 @@
 'use strict';
 
 require('dotenv').config();
-const express = require ('express');
-const cors = require('cors') ;
-
-const PORT = process.env.PORT || 3000 ;
-const server = express ();
+const express = require('express');
+const cors = require('cors');
+const superagent = require('superagent');
+const PORT = process.env.PORT || 3000;
+const server = express();
 server.use(cors());
 
 server.use(express.static('./public'));
-server .use(express.urlencoded({extended:true}));
-server.set('view engine','ejs') ;
+server.use(express.urlencoded({ extended: true }));
+server.set('view engine', 'ejs');
 
-server.get('/',(request ,response) =>{ /////// prove the app work /////////
+server.get('/', (request, response) => {
     response.render('pages/index')
-}) ;
+});
+console.log('ffffff');
+
+// research route
+server.get('/research', booksInfo);
+
+// bookHandler  function
+// function bookHandler (request,response){
+//     // console.log('dddddddd',request.data)
+//     booksInfo(request.data)
+
+// .then(book=> response.status(200).json(book))}
 
 
-server.listen(PORT , ()=> console.log (`listening to ${PORT}`)) ;
+// API
+function booksInfo(data) {
+    let url = 'https://www.googleapis.com/books/v1/volumes?q=dan';
+    console.log('urllllllllllllllllllllllllllllll', url)
+     superagent.get(url)
+    console.log('sssssssssssssssssssssssurllllllllllllllllllllllllllllll', superagent.get(url))
+        .then(books=> {
+            // console.log('gggggggggggggggg', books);
+
+            return new Books(books.volumeInfo)
+
+        })
+        // .then(book => response.status(200).json(book))
+};
+
+
+//constructure function
+function Books(title) {
+    this.image = books.volumeInfo.imageLinks.thumbnail;
+    this.title = books.volumeInfo.title;
+    this.author_name = books.volumeInfo.authors;
+    this.description = books.volumeInfo.description;
+
+}
+
+server.listen(PORT, () => console.log(`listening to ${PORT}`));
