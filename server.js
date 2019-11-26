8b5cedcd35c5ed2609c3da0c4045399a5798e0b1
muna alshorman <munaalshorman123@gmail.com>
2019-11-26 13:13:16 +0200

    do lab 11

--- server.js
+++ server.js
@@ -13,38 +13,64 @@ server.use(express.static('./public'));
 server.use(express.urlencoded({ extended: true }));
 server.set('view engine', 'ejs');
 
-server.get('/', (request, response) => {
+server.get('/searches', (request, response) => {
     response.render('pages/index')
 });
 // console.log('ffffff');
 
 // research route
-server.get('/research', booksHandler);
+server.post('/searches', (request, response) => {
+
+    let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+    if (request.body.choose === 'title') {
+        url = url + request.body.search;
+    } else if (request.body.choose === 'author') {
+        url = url + request.body.search;
+    }
+
+
+
+    superagent.get(url)
+        .then(data => {
+            // res.json(data.body)
+            //  console.log(data);
+            let books = data.body.items;
+            response.render('./pages/result', { 'x': books })
+
+            // return books.items.map(book => {
+            //     return new Book(book);
+        });
+});
 // server.post('/research', booksHandler);
 
 
 
-function booksHandler(request, response) {
-    getBooks(request.query.data)
-        .then(booksData => response.status(200).json(booksData))
-      .then(response.redirect('/result.ejs'));  
-};
-function getBooks(data) {
-    let url = 'https://www.googleapis.com/books/v1/volumes?q=books';
-    return superagent.get(url)
-        .then(data => {
-            // res.json(data.body)
-            // console.log(data);
-            let books = data.body;
-            return books.items.map(book => {
-                return new Book(book);
-            })
-        })}
+// function booksHandler(request, response) {
+//     getBooks(request.query.data)
+//         .then(booksData => response.status(200).json(booksData))
+//       .then(response.render('./pages/result'));  
+// };
+
+///////// constructor Function///////////
+
 function Book(data) {
-    this.id= data.id;
-    this.image_url = data.volumeInfo.imageLinks.thumbnail;
+    this.type = data.volumeInfo.industryIdentifiers.type || "nothing to show";
+    this.image_url = data.volumeInfo.imageLinks.thumbnail || "nothing to show";
     this.title = data.volumeInfo.title;
-    this.author = data.volumeInfo.authors;
-    this.description = data.volumeInfo.description
+    this.author = data.volumeInfo.authors[0];
+    this.description = data.volumeInfo.description || "nothing to show";
 }
+
+
+///////////////Errors////////////////////
+server.get('/boo', (request, response) => {
+    throw new Error('whoops');
+});
+server.use('*', (request, response) => {
+    response.status(404).send("Not Found");
+});
+server.use((error, request, response) => {
+    response.status(500).send("error");
+});
+// /////////// listen to server///////////////
 server.listen(PORT, () => console.log(`listening to ${PORT}`));
